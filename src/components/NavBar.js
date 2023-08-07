cb8f57e090c890493085a833792545674bafc1ac
Ayush Ray <rayayush@gmail.com>
2023-08-07 16:49:38 +0530

    Links-added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="in/aditya-sahu-192a79268"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.instagram.com/_.adi19/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
